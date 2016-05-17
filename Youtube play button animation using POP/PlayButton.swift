d5b19cbf2289619ef95d982ea67fdf74d3256ccc
这块显卡有点冷 <root@dowhile.net>
2016-05-17 21:47:09 +0800

    successful

--- Youtube play button animation using POP/PlayButton.swift
+++ Youtube play button animation using POP/PlayButton.swift
@@ -7,16 +7,95 @@
 //
 
 import UIKit
+import pop
 
-enum PlayButtonState {
-    case Paused
-    case Playing
-    
-    var value: CGFloat {
-        return (self == .Paused) ? 1.0 : 0.0
-    }
+enum PlayButtonState: CGFloat {
+    case Paused = 1.0
+    case Playing = 0.0
 }
 
 class PlayButton: UIButton {
-
+    // MARK: Vars
+    private(set) var buttonState = PlayButtonState.Paused
+    private var animationValue: CGFloat = 1.0 {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    // MARK: Methods
+    func setButtonState(buttonState: PlayButtonState, animated: Bool) {
+        // 1
+        if self.buttonState == buttonState {
+            return
+        }
+        self.buttonState = buttonState
+        
+        // 2
+        if pop_animationForKey("animationValue") != nil {
+            pop_removeAnimationForKey("animationValue")
+        }
+        
+        // 3
+        let toValue: CGFloat = buttonState.rawValue
+        
+        // 4
+        if animated {
+            let animation: POPBasicAnimation = POPBasicAnimation()
+            if let property = POPAnimatableProperty.propertyWithName("animationValue", initializer: { (prop: POPMutableAnimatableProperty!) -> Void in
+                prop.readBlock = { (object: AnyObject!, values: UnsafeMutablePointer<CGFloat>) -> Void in
+                    if let button = object as? PlayButton {
+                        values[0] = button.animationValue
+                    }
+                }
+                prop.writeBlock = { (object: AnyObject!, values: UnsafePointer<CGFloat>) -> Void in
+                    if let button = object as? PlayButton {
+                        button.animationValue = values[0]
+                    }
+                }
+                prop.threshold = 0.01
+            }) as? POPAnimatableProperty {
+                animation.property = property
+            }
+            
+            animation.fromValue = NSNumber(float: Float(self.animationValue))
+            animation.toValue = NSNumber(float: Float(toValue))
+            animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+            animation.duration = 0.25
+            pop_addAnimation(animation, forKey: "percentage")
+        } else {
+            animationValue = toValue
+        }
+    }
+    
+    
+    // MARK: Draw
+    override func drawRect(rect: CGRect) {
+        super.drawRect(rect)
+        
+        // 1
+        let height = rect.height
+        let minWidth = rect.width * 0.32
+        let aWidth = (rect.width / 2.0 - minWidth) * animationValue
+        let width = minWidth + aWidth
+        let h1 = height / 4.0 * animationValue
+        let h2 = height / 2.0 * animationValue
+        
+        // 2
+        let context = UIGraphicsGetCurrentContext()
+        
+        // 3
+        CGContextMoveToPoint(context, 0.0, 0.0)
+        CGContextAddLineToPoint(context, width, h1)
+        CGContextAddLineToPoint(context, width, height - h1)
+        CGContextAddLineToPoint(context, 0.0, height)
+        CGContextMoveToPoint(context, rect.width - width, h1)
+        CGContextAddLineToPoint(context, rect.width, h2)
+        CGContextAddLineToPoint(context, rect.width, height - h2)
+        CGContextAddLineToPoint(context, rect.width - width, height - h1)
+        
+        // 4
+        CGContextSetFillColorWithColor(context, tintColor.CGColor)
+        CGContextFillPath(context)
+    }
 }
--- Youtube play button animation using POP/ViewController.swift
+++ Youtube play button animation using POP/ViewController.swift
@@ -10,14 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var playButton: PlayButton!
+    
+    // MARK: -
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        playButton = PlayButton()
+        playButton.frame = CGRect(x: floor((view.bounds.width - 150.0) / 2.0), y: 50.0, width: 150.0, height: 150.0)
+        playButton.addTarget(self, action: #selector(ViewController.playButtonPressed), forControlEvents: .TouchUpInside)
+        view.addSubview(playButton)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: -
+    // MARK: Methods
+    
+    func playButtonPressed() {
+        if playButton.buttonState == .Playing {
+            playButton.setButtonState(.Paused, animated: true)
+        } else {
+            playButton.setButtonState(.Playing, animated: true)
+        }
     }
 
 
